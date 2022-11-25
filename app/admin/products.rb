40afb08ba2fde96c9d25006857f6cca710543cc7
pham-hung-25-03-01 <phamhunggl721@gmail.com>
2022-11-25 11:28:01 +0700

    admin product show view coding

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,30 +1,52 @@
 ActiveAdmin.register Product do
-    remove_filter :reviews
     permit_params :event_id, :supplier_id, :product_group_id, :category_id, :age_id, :product_name, :origin, :description,
     :gender, :warranty, :import_price, :sell_price, :product_discount, :shipping, :score_rating, :number_of_rates, :is_available
   
     index do
       selectable_column
       id_column
-      column :product_group_name
-      column :supplier_id
-      column :category_id
-      column :age_id
+      column :product_name
+      column 'Image' do |item|
+        unless item.product_images.nil?
+          image_url = item.product_images.find_by(is_default: true).image_url
+          link_to(image_tag(image_url, width: '70px', height: '70px'), image_url, target: '_blank')
+        end
+      end
+      column :supplier_id do |item|
+        item.supplier.nil? ? '' : item.supplier.supplier_name
+      end
+      column :category_id do |item|
+        item.category.nil? ? '' : item.category.category_name
+      end
+      column :age_id do |item|
+        item.age.nil? ? '' : item.age.age_name
+      end
       column :origin
-      column :gender
+      column :gender do |item|
+        item.gender ? status_tag('Male', class: 'yes') : status_tag('Female', class: 'no')
+      end
       column :warranty
-      column :import_price
-      column :sell_price
-      column :product_discount
-      column :score_rating
+      column :sell_price do |item|
+        number_to_currency(item.sell_price, precision: 0, unit: '')
+      end
+      column :product_discount do |item|
+        item.product_discount.round(1)
+      end
+      column :score_rating do |item|
+        item.score_rating.round(1)
+      end
       actions
     end
-    # filter :supplier_name
-    # filter :contract_date
-    # filter :phone_number
-    # filter :email
-    # filter :address
-    # filter :is_cooperated
+    filter :product_name
+    filter :supplier, as: :select, collection: -> { Supplier.where(is_actived: true).pluck(:supplier_name, :id) }
+    filter :category, as: :select, collection: -> { Category.where(is_actived: true).pluck(:category_name, :id) }
+    filter :age, as: :select, collection: -> { Age.where(is_actived: true).pluck(:age_name, :id) }
+    filter :origin
+    filter :gender, as: :select, collection: -> { [['Male', true], ['Female', false]] }
+    filter :warranty, as: :numeric
+    filter :sell_price, as: :numeric
+    filter :product_discount, as: :numeric
+    filter :score_rating, as: :numeric
   
     # form do |f|
     #   f.semantic_errors
@@ -67,27 +89,49 @@ ActiveAdmin.register Product do
     #   end
     #   redirect_to collection_path, notice: "Successfully deleted #{ids.count} supplier#{ids.count > 1 ? 's' : ''}"
     # end
-    # show do |supplier|
-    #   attributes_table do
-    #     row :id
-    #     row :supplier_name
-    #     row :contract_date
-    #     row :phone_number
-    #     row :email
-    #     row :address
-    #     row :is_cooperated
-    #     row :created_at
-    #     row :updated_at
-    #     row :created_by do
-    #       admin_user = AdminUser.find(supplier.created_by)
-    #       link_to admin_user.email, admin_admin_user_path(admin_user.id)
-    #     end
-    #     row :updated_by do
-    #       admin_user = AdminUser.find(supplier.updated_by)
-    #       link_to admin_user.email, admin_admin_user_path(admin_user.id)
-    #     end
-    #   end
-    # end
+    show do |product|
+      attributes_table do
+        row :product_name
+        #row :product_images
+        row :product_group do
+          product_group = ProductGroup.find_by(id: product.product_group_id)
+          link_to product_group.product_group_name, admin_product_group_path(product.product_group_id) unless product_group.nil?
+        end
+        row :supplier do
+          supplier = Supplier.find_by(id: product.supplier_id)
+          link_to supplier.supplier_name, admin_supplier_path(product.supplier_id) unless supplier.nil?
+        end
+        row :category do
+          category = Category.find_by(id: product.category_id)
+          link_to category.category_name, admin_category_path(product.category_id) unless category.nil?
+        end
+        row :age do
+          age = Age.find_by(id: product.age_id)
+          link_to age.age_name, admin_age_path(product.age_id) unless age.nil?
+        end
+        row :event do
+          event = Event.find_by(id: product.event_id)
+          link_to event.event_name, admin_age_path(product.event_id) unless event.nil?
+        end
+        row :gender do
+          product.gender ? status_tag('Male', class: 'yes') : status_tag('Female', class: 'no')
+        end
+        row :origin
+        row :description
+        row :warranty
+        
+        row :created_at
+        row :updated_at
+        row :created_by do
+          admin_user = AdminUser.find(product.created_by)
+          link_to admin_user.email, admin_admin_user_path(admin_user.id)
+        end
+        row :updated_by do
+          admin_user = AdminUser.find(product.updated_by)
+          link_to admin_user.email, admin_admin_user_path(admin_user.id)
+        end
+      end
+    end
     # before_create do |supplier|
     #   supplier.created_by = current_admin_user.id
     #   supplier.updated_by = current_admin_user.id
